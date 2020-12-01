eca1cbc8116fd4c1be2025d114145277e94664d5
Bruno Rodrigues Dos Reis Rossi Serra <74634382+brunoserra@users.noreply.github.com>
2020-12-01 01:11:30 -0300

    Create arrowfunctions.js

---
+++ arrowfunctions.js
@@ -0,0 +1,13 @@
+//arrow function o que é
+//é uma abstração de uma funçao
+//=> operador de arrow 
+const multiplica = function(x, y) {
+        return x * y;
+    }
+    //com arrow 
+const dividir = (x, y) => {
+        return x / y;
+    }
+    //mais uma contruçõao serve para objetos 
+    //deve encapsular com ({})
+const objetos = () => ({});
