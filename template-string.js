5a6c3a8d24cfabf240a83f46a5bb104ee4723755
Bruno Rodrigues Dos Reis Rossi Serra <74634382+brunoserra@users.noreply.github.com>
2020-12-01 00:26:19 -0300

    Update template-string.js

--- template-string.js
+++ template-string.js
@@ -8,4 +8,10 @@ let minhaProfissao = "bancario"
 //template string usa``para iniciar 
 console.log(`ola meu nome é ${meuNome} ${meuSobrenome} ${minhaProfissao}`); //é possivel fazeroperações dentro do template  template string
 
-console.log(` soma 1 + 1 é igual a`);
+console.log(` soma 1 + 1 é igual a ${ 1 + 1 }`);
+console.log(`o objeto jason ${{ chave: 'vvalor' }}`);
+
+let meuPbjeto = {
+        chave: 'novo valor'
+    }
+    // declaração a partir de chaves= forma literal
