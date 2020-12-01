c2a4e815142332ef91e55e2bb70d0a3aac217087
Bruno Rodrigues Dos Reis Rossi Serra <74634382+brunoserra@users.noreply.github.com>
2020-12-01 00:44:43 -0300

    Create operador-ternario.js

---
+++ operador-ternario.js
@@ -0,0 +1,15 @@
+//ternario serve parafacilitartomada de decisões
+// caso contratio usaria !isValid
+let isValid = true;
+
+function verify(isValid) {
+    if (isValid) {
+        return true;
+    } else {
+        return false;
+    }
+    console.log(verify(isValid));
+}
+console.log(verify(isValid));
+// resumir essa funçaõ em uma linha de comando
+// com ternario
