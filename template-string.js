8360f48d6957cbad6f0af13c2f4824af21e54b2c
Bruno Rodrigues Dos Reis Rossi Serra <74634382+brunoserra@users.noreply.github.com>
2020-11-30 23:33:00 -0300

    Update template-string.js

--- template-string.js
+++ template-string.js
@@ -1,4 +1,11 @@
 // forma moderna de delcarar variavel é com let
+// boas praticas e atalhos no javascript
 let meuNome = "Bruno";
 let meuSobrenome = "serra";
 let minhaProfissao = "bancario"
+
+//sem template string o codigo fica longo
+//template string usa``para iniciar 
+console.log(`ola meu nome é ${meuNome} ${meuSobrenome} ${minhaProfissao}`); //é possivel fazeroperações dentro do template  template string
+
+console.log(` soma 1 + 1 é igual a`);
