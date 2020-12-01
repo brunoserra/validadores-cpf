36fd4030879e02709ed3473d287e7a3858dfb3ce
Bruno Rodrigues Dos Reis Rossi Serra <74634382+brunoserra@users.noreply.github.com>
2020-11-30 21:22:50 -0300

    aprendendo

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("JavaScript carregado");
+console.log("Jav script carregado");
+
+//criar função
+
+//encapsular funçao
+
+function validaCpf(cpf) {
+
+}
+
+function validar() {
+    console.log("Iniciando validação CPF");
+    var cpf = document.getElementById('cpf_digitado').value;
+    var resultadoValidacao = validaCpf(cpf);
+    if (resultadoValidacao) {
+        document.getElementById('sucess').style.display = block;
+
+    }
+
+
+
+}
