cd32a89894c020ebae97c6a21affc4e55c7e3318
Bruno Rodrigues Dos Reis Rossi Serra <74634382+brunoserra@users.noreply.github.com>
2020-12-01 22:20:43 -0300

    Create spread.js

---
+++ spread.js
@@ -0,0 +1,10 @@
+let lista = [1, 2.5];
+let listaIncluir = [3, 4];
+
+
+for (let i = 0; i < lista.length; i++) {
+    for (let j = 0; j < lista.length; j++)
+        if (lista[j] >= listaIncluir[i]) {
+            lista.splice(j, 0, listaIncluir[i])
+        }
+}
