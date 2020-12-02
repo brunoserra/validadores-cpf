03ab204c2055114b22d2dbcbdd95cb5e10af1f15
Bruno Rodrigues Dos Reis Rossi Serra <74634382+brunoserra@users.noreply.github.com>
2020-12-01 23:54:55 -0300

    Update match.js

--- match.js
+++ match.js
@@ -1,4 +1,11 @@
 // interessante para buscar conteudos especificos
-const cpf = 'meu cef é 123.456.321.44';
+const cpf = 'meu cef é 123.456.321-44';
 
-const regex = new RegExp()
+
+//expressão regular
+const regex = new RegExp('[0-9]{3}.[0-9]{3}.[0-9]{3}-[0-9]{2}');
+
+console.log(cpf.match(regex));
+
+
+//rever o codigo esta dando null
