7a53f7e0c0478946135af810788a7aa3e1df2bf4
Bruno Rodrigues Dos Reis Rossi Serra <74634382+brunoserra@users.noreply.github.com>
2020-12-01 22:34:42 -0300

    Update spread.js

--- spread.js
+++ spread.js
@@ -2,11 +2,16 @@ let lista = [1, 2, 5];
 let listaIncluir = [3, 4];
 
 
+
 for (let i = 0; i < lista.length; i++) {
-    for (let j = 0; j < lista.length; j++)
+    for (let j = 0; j < lista.length; j++) {
         if (lista[j] >= listaIncluir[i]) {
             lista.splice(j, 0, listaIncluir[i]);
             j++;
         }
+    }
 }
 console.log(lista);
+
+
+//outra forma mais simples de fazer
