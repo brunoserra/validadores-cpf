3bb56a98bce9ba3bc853cb3ab385d2fc4f12b6b0
Bruno Rodrigues Dos Reis Rossi Serra <74634382+brunoserra@users.noreply.github.com>
2020-12-01 22:52:45 -0300

    Update spread.js

--- spread.js
+++ spread.js
@@ -1,19 +1,18 @@
-let lista = [1, 2, 5];
-let listaIncluir = [3, 4];
+//let lista = [1, 2, 5];
+//let listaIncluir = [3, 4];
 
 
 
-for (let i = 0; i < lista.length; i++) {
-    for (let j = 0; j < lista.length; j++) {
-        if (lista[j] >= listaIncluir[i]) {
-            lista.splice(j, 0, listaIncluir[i]);
-            j++;
-        }
-    }
-}
+//for (let i = 0; i < lista.length; i++) {
+//for (let j = 0; j < lista.length; j++) {
+//if (lista[j] >= listaIncluir[i]) {
+//lista.splice(j, 0, listaIncluir[i]);
+//j++;}}}
 
 
 
 //outra forma mais simples de fazer 
-const listaResult = [1, 2, ...listaIncluir, 5];
-console.log(listaResult);
+//const listaResult = [1, 2, ...listaIncluir, 5];
+//console.log(listaResult);
+let arr = ['a', 'b', 'c'];
+let arr2 = arr;
