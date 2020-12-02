2ee008f4a708d6baa99031d7ac7e242d4ae953ae
Bruno Rodrigues Dos Reis Rossi Serra <74634382+brunoserra@users.noreply.github.com>
2020-12-01 23:25:56 -0300

    Update Desestruct_match.js

--- Desestruct_match.js
+++ Desestruct_match.js
@@ -6,4 +6,13 @@ const pessoa = {
 
 
 };
-console.log(pessoa);
+
+//forma que seria feito pela mairoria mais dificl
+//let nome = pessoa.nome;
+//let sobreNome = pessoa.sobreNome;
+//let idade = pessoa.idade;
+//let profissao = pessoa.profisao;
+
+//modo novo valor função destruction
+let { nome, sobreNome, idade, profisao } = pessoa;
+console.log(nome, sobreNome, idade, profisao);
