377210850756d415445cee8e44f5969bd07d6cc4
Bruno Rodrigues Dos Reis Rossi Serra <74634382+brunoserra@users.noreply.github.com>
2020-12-01 23:00:13 -0300

    Update spread.js

--- spread.js
+++ spread.js
@@ -15,7 +15,7 @@
 //const listaResult = [1, 2, ...listaIncluir, 5];
 //console.log(listaResult);
 let arr = ['a', 'b', 'c'];
-let arr2 = arr;
+let arr2 = [...arr];
 arr2.push('d');
 
-console.log(`Arr = ${arr}`);
+console.log(`Arr = ${arr2}`);
